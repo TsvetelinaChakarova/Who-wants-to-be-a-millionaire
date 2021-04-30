ba5799c10ed68a531b736fbe2fa27b92a571c8b4
TsvetelinaChakarova <64734270+TsvetelinaChakarova@users.noreply.github.com>
2021-04-30 18:37:07 +0300

    Update AddQuestion.cpp

--- AddQuestion.cpp
+++ AddQuestion.cpp
@@ -1,17 +1,4 @@
-/**
-*
-* Solution to course project #1
-* Introduction to programming course
-* Faculty of Mathematics and Informatics of Sofia University
-* Winter semester 2020/2021
-*
-* @author Tsvetelina Chakarova
-* @idnumber 62553
-* @compiler VC
-*
-* .cpp  file with definitions for the functions in AddQuestion.h
-*
-*/
+//@author Tsvetelina Chakarova
 
 #include <iostream>
 #include <fstream>
