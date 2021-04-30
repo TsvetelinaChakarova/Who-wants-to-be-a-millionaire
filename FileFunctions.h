c4a5cfc6b290abf540820febfe65c9d47126ab9e
TsvetelinaChakarova <64734270+TsvetelinaChakarova@users.noreply.github.com>
2021-04-30 18:39:31 +0300

    Update FileFunctions.h

--- FileFunctions.h
+++ FileFunctions.h
@@ -1,14 +1,6 @@
-/**
-*
-* Solution to course project #1
-* Introduction to programming course
-* Faculty of Mathematics and Informatics of Sofia University
-* Winter semester 2020/2021
-*
+/*
 * @author Tsvetelina Chakarova
-* @idnumber 62553
 * @compiler VC
-*
 * .h file containing the functions connected to the direct work with txt files
 *
 */
