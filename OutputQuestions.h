f8961b450833d49471859214ff9380a57fa61407
TsvetelinaChakarova <64734270+TsvetelinaChakarova@users.noreply.github.com>
2021-04-30 18:41:49 +0300

    Update OutputQuestions.h

--- OutputQuestions.h
+++ OutputQuestions.h
@@ -1,14 +1,6 @@
-/**
-*
-* Solution to course project #1
-* Introduction to programming course
-* Faculty of Mathematics and Informatics of Sofia University
-* Winter semester 2020/2021
-*
+/*
 * @author Tsvetelina Chakarova
-* @idnumber 62553
 * @compiler VC
-*
 * .h file containing the functions related to outputing questions for option 1. New game from the home menu
 *
 */
