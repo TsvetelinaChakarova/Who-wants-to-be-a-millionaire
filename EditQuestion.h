eed31ab43879cbc730d624a01f7aeb14e44f47a2
TsvetelinaChakarova <64734270+TsvetelinaChakarova@users.noreply.github.com>
2021-04-30 18:38:55 +0300

    Update EditQuestion.h

--- EditQuestion.h
+++ EditQuestion.h
@@ -1,14 +1,6 @@
-/**
-*
-* Solution to course project #1
-* Introduction to programming course
-* Faculty of Mathematics and Informatics of Sofia University
-* Winter semester 2020/2021
-*
+/*
 * @author Tsvetelina Chakarova
-* @idnumber 62553
 * @compiler VC
-*
 * .h  file containing the function EditQuestion() that is called when the user chooses 3. Edit question from the Home Menu
 *
 */
